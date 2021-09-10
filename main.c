b7a9e378efe8d4cba14a0479994445f4f68b7c9a
troclaux <arthur.albuquerque.1995@gmail.com>
2021-09-09 21:58:12 -0300

    + processo

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int seed = 48271; // http://c-faq.com/lib/rand.html
+srand((time(NULL)*seed)); // should be called just once on the entire code base
+
+int main(){
+  
+}
---
+++ process.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+typedef struct process {
+  int pid;
+  int ppid;
+  int start;
+  int end;
+  int priority;
+  int status;
+  int processedTU;
+  int service;
+  int *printerRequests;
+  int *tapeRequests;
+  int *diskRequests;
+} Process;
+
+bool checkIntInArray(int number, int *array) {
+  int i, length;
+  length = array[0];
+  for (i = 1; i < length; i++) {
+    if (array[i] == number) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int *generateIOTime(int serviceTime, int *IOArray1, int *IOArray2) {
+  bool exists;
+  int i, temp;
+  int *IOTimeArray;
+  int quantity = rand() % 4;
+  IOTimeArray = (int *)calloc(quantity + 1, sizeof(int));
+  IOTimeArray[0] = quantity;
+  for (i = 1; i < quantity + 1; i++) {
+    temp = rand() % serviceTime;
+    if (checkIntInArray(temp, IOTimeArray) || 
+        checkIntInArray(temp, IOArray1) || 
+        checkIntInArray(temp, IOArray2)) {
+      exists = true;
+      while (exists) {
+        temp = rand() % serviceTime;
+        if (checkIntInArray(temp, IOTimeArray) || 
+            checkIntInArray(temp, IOArray1) || 
+            checkIntInArray(temp, IOArray2)) {
+          continue;
+        }
+        exists = false;
+      }
+    }
+    IOTimeArray[i] = temp;
+  }
+  return IOTimeArray;
+}
+
+Process newProcess(int pid, int ppid) {
+  Process process;
+  process.pid = pid;
+  process.ppid = ppid;
+  process.start = rand() % 30;
+  process.end = -1;
+  process.processedTU = 0;
+
+  process.priority = 1;
+  process.status = 0;
+  process.service = rand() % 30;
+  process.diskRequests = generateIOTime(process.service, process.tapeRequests, process.printerRequests);
+  process.tapeRequests = generateIOTime(process.service, process.diskRequests, process.printerRequests);
+  process.printerRequests = generateIOTime(process.service, process.diskRequests, process.tapeRequests);
+
+  return process;
+}
+
+void printProcess(Process process){
+  printf("PID | PPID | status | start | end | priority | processedTU | service | printer| tape | disk \n");
+  printf("%d | ", process.pid);
+  printf("%d | ", process.ppid);
+  printf("%d | ", process.status);
+  printf("%d | ", process.start);
+  printf("%d | ", process.end);
+  printf("%d | ", process.priority);
+  printf("%d | ", process.processedTU);
+  printf("%d | ", process.service);
+  printf("%p | ", (void *) process.printerRequests);
+  printf("%p | ", (void *) process.tapeRequests);
+  printf("%p \n", (void *) process.diskRequests);
+}
